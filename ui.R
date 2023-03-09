1d1c800071d7cd56fde24c62cbfa811f5a014758
zasteers@uw.edu <zasteers@uw.edu>
2023-03-08 20:22:07 -0800

    intro

--- ui.R
+++ ui.R
@@ -67,7 +67,9 @@ ggplot(events_and_volunteers_per_country, aes(x = num_events, y = num_volunteers
 # Application title
 intro_panel <- tabPanel(
   "Introduction",
-  fluidPage()
+  fluidPage(
+    includeMarkdown("intro.Rmd")
+  )
 )
 # An introductory page that provides an overview of the project. What major questions are you seeking to answer? What data are you using to answer those questions? Please provide a URL link to the original source(s) of the data. Where did the data come from, and what are possible ethical questions or limitations to consider with this dataset? You should also include some additional "flare" on this landing page, such as an image.
 
