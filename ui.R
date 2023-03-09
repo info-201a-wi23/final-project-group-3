9e4e310a88bd4bc1e8917dc9d915d40e76e81b5c
zasteers@uw.edu <zasteers@uw.edu>
2023-03-08 23:23:10 -0800

    last push

--- ui.R
+++ ui.R
@@ -62,10 +62,6 @@ ggplot(events_and_volunteers_per_country, aes(x = num_events, y = num_volunteers
   scale_x_continuous(breaks = seq(0, max(events_and_volunteers_per_country$num_events), by = 10000)) +
   scale_y_continuous(breaks = seq(0, max(events_and_volunteers_per_country$num_volunteers), by = 300000))
 
-my_theme <- bs_theme(bg = "#C9F0FF",
-                     fg = "black")
-                     
-
 # Application title
 intro_panel <- tabPanel(
   "Introduction",
@@ -195,8 +191,7 @@ conclusion_panel <- tabPanel(
 # A conclusion page of summary takeaways that hones in on at least 3 major takeaways from the project, which should be related to a specific aspect of your analysis. This page should include at least 250 words of text, but feel free to incorporate tables, graphics, or other elements necessary to convey your conclusions.
 
 ui <- navbarPage(
-  theme = my_theme,
-  "Plastic Polution Around the World",
+  "Plastic Pollution Around the World",
   intro_panel,
   plot_panel,
   plot_panel_2,
